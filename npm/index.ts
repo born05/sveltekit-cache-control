6d0693343dbb0e29382ed9ce8f8475e74bf296d9
Niels <nielsjlwijers@gmail.com>
2024-07-10 09:10:33 +0200

    feat: more options + standalone function for apis

--- npm/index.ts
+++ npm/index.ts
@@ -5,9 +5,11 @@ import { captureException } from '@sentry/sveltekit';
 interface Options {
   enabled: boolean;
   mustRevalidate: boolean;
-  maxAge: number;
+  maxAge: number | null;
+  sMaxAge: number | null;
   public: boolean;
   private: boolean;
+  noCache: boolean;
   routes: string[];
   methods: string[];
   noCacheSearchParams: string[];
@@ -18,81 +20,124 @@ const DEFAULT_OPTIONS: Options = {
   enabled: true,
   mustRevalidate: false,
   maxAge: 60,
+  sMaxAge: null,
   public: true,
   private: false,
+  noCache: false,
   routes: ['.*'],
   methods: ['GET'],
   noCacheSearchParams: ['preview'],
   etagCacheKey: 'cache-control-etag',
 };
 
-export function cacheControlHandle(
-  redisUrl: string,
-  opt: Partial<Options>
-): Handle {
-  const options = {
-    ...DEFAULT_OPTIONS,
-    ...opt,
-  };
+class CacheControl {
+  redis: Redis | null = null;
 
-  if (!options.enabled) {
-    return async ({ event, resolve }) => resolve(event);
+  constructor() {
+    if (process.env.REDIS_URL) {
+      this.initRedis(process.env.REDIS_URL);
+    }
   }
 
-  const redis = new Redis(redisUrl);
+  private initRedis(redisUrl: string) {
+    this.redis = new Redis(redisUrl);
 
-  redis.on('error', (error) => {
-    console.error(error);
-    captureException(error);
-  });
+    this.redis.on('error', (error) => {
+      console.error(error);
+      captureException(error);
+    });
 
-  redis.on('connect', () => {
-    console.log('Redis connected');
-  });
+    this.redis.on('connect', () => {
+      console.log('Redis connected');
+    });
+  }
 
-  return async function ({ event, resolve }) {
-    const response = await resolve(event);
+  public async createResponse(
+    redisUrl: string,
+    opt: Partial<Options>,
+    request: Request,
+    response: Response = new Response()
+  ) {
+    const options = {
+      ...DEFAULT_OPTIONS,
+      ...opt,
+    };
 
-    if (
-      response.status === 200 &&
-      options.methods.includes(event.request.method) &&
-      !event.request.headers.has('Authorization') &&
-      options.routes.some((route) =>
-        new RegExp(route).test(event.url.pathname)
-      ) &&
-      options.noCacheSearchParams.every(
-        (param) => !event.url.searchParams.has(param)
-      )
-    ) {
-      response.headers.set(
-        'Cache-Control',
-        [
-          !options.public && options.private && 'private',
-          options.public && !options.private && 'public',
-          `max-age=${options.maxAge}`,
-          options.mustRevalidate && 'must-revalidate',
-        ]
-          .filter(Boolean)
-          .join(', ')
-      );
+    if (!this.redis) {
+      this.initRedis(redisUrl);
+    }
 
-      if (options.etagCacheKey && redis) {
-        const etag = await redis.get(options.etagCacheKey);
+    response.headers.set(
+      'Cache-Control',
+      [
+        options.private && 'private',
+        options.public && 'public',
+        options.maxAge !== null && `max-age=${options.maxAge}`,
+        options.sMaxAge !== null && `s-maxage=${options.maxAge}`,
+        options.mustRevalidate && 'must-revalidate',
+      ]
+        .filter(Boolean)
+        .join(', ')
+    );
 
-        if (etag) {
-          response.headers.set('ETag', etag);
+    if (options.etagCacheKey && this.redis) {
+      const etag = await this.redis.get(options.etagCacheKey);
 
-          const requestEtag = event.request.headers.get('If-None-Match');
-          if (requestEtag === etag) {
-            return new Response(null, {
-              status: 304,
-              statusText: 'Not Modified',
-            });
-          }
+      if (etag) {
+        response.headers.set('ETag', etag);
+
+        const requestEtag = request.headers.get('If-None-Match');
+        if (requestEtag === etag) {
+          return new Response(null, {
+            status: 304,
+            statusText: 'Not Modified',
+          });
         }
       }
     }
+  }
 
-    return response;
-  } satisfies Handle;
+  public cacheControlHandle(redisUrl: string, opt: Partial<Options>): Handle {
+    const options = {
+      ...DEFAULT_OPTIONS,
+      ...opt,
+    };
+
+    if (!options.enabled) {
+      return async ({ event, resolve }) => resolve(event);
+    }
+
+    if (!this.redis) {
+      this.initRedis(redisUrl);
+    }
+
+    return async ({ event, resolve }) => {
+      const response = await resolve(event);
+
+      if (
+        response.status === 200 &&
+        options.methods.includes(event.request.method) &&
+        !event.request.headers.has('Authorization') &&
+        options.routes.some((route) =>
+          new RegExp(route).test(event.url.pathname)
+        ) &&
+        options.noCacheSearchParams.every(
+          (param) => !event.url.searchParams.has(param)
+        )
+      ) {
+        const resp = await this.createResponse(
+          redisUrl,
+          options,
+          event.request,
+          response
+        );
+
+        if (resp) return resp;
+      }
+
+      return response;
+    };
+  }
 }
+
+export default new CacheControl();
